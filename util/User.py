88dafe2bca13c435287c84968f481a96a64e71ec
Ran Liu <xabsox@gmail.com>
2019-11-18 13:08:13 -0500

    Update requirements.

--- util/User.py
+++ util/User.py
@@ -1,6 +1,7 @@
 class User:
-    def __init__(self, id, username, password):
+    def __init__(self, id, username, password_hash, salt):
         # TODO authentication
         self.id = id
         self.username = username
-        self.password = password
+        self.password_hash = password_hash
+        self.salt = salt
