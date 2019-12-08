a1a7abd6db4cb485d8a19a92f9c166720fe56855
Ran Liu <xabsox@gmail.com>
2019-12-08 18:12:20 -0500

    Controller

--- controller/HoistController.py
+++ controller/HoistController.py
@@ -1,3 +1,9 @@
+import jwt
+import config
+import datetime
+from model.Hoist import Hoist
+
+
 class HoistController:
     def __init__(self, hoistDAO, userDAO, sessionDAO):
         self.hoistDAO = hoistDAO
@@ -10,10 +16,22 @@ class HoistController:
         weight = request['weight']
         reps = request['weight']
         # Check that auth token is valid
-        # Check if there are any recent hoists. (30 minutes)
-        # If there are recent hoists, append to that hoist's session
-        # Else, create new session
-        return
+        try:
+            decoded_token = jwt.decode(request['token'], config.secret_key, algorithms='HS256')
+            user = self.userDAO.get(decoded_token['user'])
+            if user is None:
+                return {'error': 'User not found'}, 401
+            if decoded_token['issued'] < (datetime.datetime.utcnow() - datetime.timedelta(days=1)).timestamp():
+                return {'error': 'Token expired'}, 401
+
+            # Check if there are any recent hoists. (30 minutes)
+
+            # If there are recent hoists, append to that hoist's session
+
+            # Else, create new session
+            return {'username': user.username, 'id': user.id}, 200
+        except jwt.DecodeError:
+            return {'error': 'Invalid token'}, 401
 
     def update(self, request):
         token = request['token']
