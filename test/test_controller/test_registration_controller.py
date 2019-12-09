e5bc9271c34309ab6bd8a5d3bdd3cfc5a2ad824b
Ran Liu <xabsox@gmail.com>
2019-12-09 12:29:06 -0500

    More unit tests.

--- test/test_controller/test_registration_controller.py
+++ test/test_controller/test_registration_controller.py
@@ -1,9 +1,34 @@
 import unittest
+import os
+import database.schema
+from controller.RegistrationController import RegistrationController
+from database.UserDAO import UserDAO
 
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, True)
+class TestRegistrationController(unittest.TestCase):
+    def setUp(self) -> None:
+        if os.path.exists("test.sqlite"):
+            os.remove("test.sqlite")
+        database.schema.init_db("test.sqlite")
+
+    def tearDown(self) -> None:
+        if os.path.exists("test.sqlite"):
+            os.remove("test.sqlite")
+
+
+    def test_registration(self):
+        userDAO = UserDAO("test.sqlite")
+        registrationController = RegistrationController(userDAO)
+
+        request = {
+            'username': 'user',
+            'password': 'password'
+        }
+
+        result = registrationController.register(request)
+
+        self.assertEqual(result[1], 200)
+
 
 
 if __name__ == '__main__':
